cd8754c49f2723ca00c0c32bab5a772067ee74ac
Abirami-2408 <rk.abirami2005@gmail.com>
2026-06-27 21:46:28 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -40,7 +40,7 @@ function loadStudents() {
         .catch(error => console.error("Error:", error));
 }
 function deleteStudent(id) {
-    fetch(`https://courses-registeration-backened.onrender.com/api/courses/${id}`), {
+    fetch(`https://courses-registeration-backened.onrender.com/api/courses/${id}`,) {
         method: "DELETE"
     }
     .then(response => response.text())
